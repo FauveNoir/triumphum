2106c3e58704632d4c3804a4a1f5893af0e679c8
FauveNoir <fauve.ordinator@taniere.info>
2025-01-28 23:18:58 +0100

    modularisation des commentaires

--- main.py
+++ main.py
@@ -52,14 +52,6 @@ verifyConfigFileExistence()
 
 
 
-########################################################################
-# Classe des commentaires
-########################################################################
-
-class Comment:
-	def __init__(self, date=None, content=None):
-		self.date = date
-		self.content = content
 
 
 
---
+++ triumphum/comments_classes.py
@@ -0,0 +1,9 @@
+
+########################################################################
+# Classe des commentaires
+########################################################################
+
+class Comment:
+	def __init__(self, date=None, content=None):
+		self.date = date
+		self.content = content
