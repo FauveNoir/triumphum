ae3dad96e5e157b7aca03859eed5902a24f0a301
FauveNoir <fauve.ordinator@taniere.info>
2025-01-29 01:55:20 +0100

    modularisation des jeux

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import webbrowser
 import pyperclip
 import re
 from datetime import date, datetime, timedelta
-from tabulate import tabulate
 import humanize
 #import pendulum
 import locale
@@ -33,11 +32,12 @@ from triumphum.keybindings import *
 from triumphum.layouts import *
 from triumphum.internal_shell_class import *
 from triumphum.descriptors import addNewGameAfterInterativeDescriptor, addNewGenreAfterInterativeDescriptor, addNewLicenceAfterInterativeDescriptor, addNewPlatformAfterInterativeDescriptor
-from triumphum.platforms_classes import create_platform_objects, get_platform_object_after_code
-from triumphum.genres_classes import create_game_genre_objects, get_genre_object_after_code
-from triumphum.licences_classes import create_licence_objects, get_licence_object_after_code
+from triumphum.platforms_classes import create_platform_objects
+from triumphum.genres_classes import create_game_genre_objects
+from triumphum.licences_classes import create_licence_objects
 from triumphum.cli_functions import *
-from triumphum.history_classes import retrive_history_of_a_game, History
+from triumphum.history_classes import  History
+from triumphum.games_classes import  create_game_objects
 from triumphum.debug import *
 
 
@@ -57,166 +57,6 @@ verifyConfigFileExistence()
 
 
 
-########################################################################
-# Classe des jeux
-########################################################################
-
-def formatDataListToLitteralList(list_, voidSymbol):
-	try:
-		n = len(list_)
-	except:
-		n = 0
-	if n == 0:
-		return voidSymbol # TODO
-	elif n == 1:
-		return list_[0]
-	elif n == 2:
-		return f"{list_[0]} et {list_[1]}"
-	else:
-		elements = ", ".join(list_[:-1])
-		return f"{elements}, et {list_[-1]}"
-
-# Défffinition de classe
-class Game:
-	def __init__(self, name=None, code=None, licence=None, url=None, year=None, genre=None, authors=None, studios=[], command=None, comments=None, platform=None):
-		self.name = name
-		self.code = code
-		self.licence = licence
-		self.url = url
-		self.year = year
-		self.genre = genre
-		self.authors = authors
-		self.studios = studios
-		self.command = command
-		self.comments = comments
-		self.platform = platform
-		self.history = self.get_history()
-		self.latest_opening_date_value = self.latest_opening_date()
-		self.playing_duration = self.cumulate_time()
-
-		listOfGames[self.code]=self # Adjonction à la liste des jeux
-
-	def ncurseLine(self):
-		# Préparation de la ligne de tableau
-
-		# Vérifier chaque clé pour une éventuelle valeur vide et remplacer par "-"
-		ncurseLine = [
-			self.platform.abbr or PLATFORM_VOID_SYMBOL.value,
-			self.name or NAME_VOID_SYMBOL.value,
-			self.licence.abbr or LICENCE_VOID_SYMBOL.value,
-			self.genre.abbr or GENRE_VOID_SYMBOL.value,
-			self.year or DATE_VOID_SYMBOL.value,
-			self.human_latest_opening_duration() or LASTOPENING_VOID_SYMBOL.value,
-			self.human_cumulate_time() or CUMULATEDTIME_VOID_SYMBOL.value,
-			self.listOfAuthors() or AUTHOR_VOID_SYMBOL.value,
-			self.listOfStudios() or STUDIO_VOID_SYMBOL.value,
-			self
-		]
-		return ncurseLine
-
-	def asciiRow(self):
-		# Vérifier chaque clé pour une éventuelle valeur vide et remplacer par "-"
-		asciiRow = [
-			self.platform.abbr or PLATFORM_VOID_SYMBOL.value,
-			self.name or NAME_VOID_SYMBOL.value,
-			self.licence.abbr or LICENCE_VOID_SYMBOL.value,
-			self.genre.abbr or GENRE_VOID_SYMBOL.value,
-			self.year or DATE_VOID_SYMBOL.value,
-			self.human_latest_opening_duration() or LASTOPENING_VOID_SYMBOL.value,
-			self.human_cumulate_time() or CUMULATEDTIME_VOID_SYMBOL.value,
-			self.listOfAuthors() or AUTHOR_VOID_SYMBOL.value,
-			self.listOfStudios() or STUDIO_VOID_SYMBOL.value
-		]
-		return asciiRow
-
-	def sheet(self):
-		# Fiche rapide de description de jeu
-		sheet_data=[
-			["Nom", self.name],
-			["code", self.code],
-			["Licence", self.licence.name],
-			["URL", self.url],
-			["Genre", self.genre.name],
-			["Auteur", self.listOfAuthors()],
-			["Commande", self.command],
-			["Dernière ouverture", self.latest_opening_date()],
-		]
-
-		print(tabulate(sheet_data))
-
-	def get_history(self):
-		# Retourne l’historique des dates et heures de parties jouées
-		return retrive_history_of_a_game(self)
-
-	def cumulate_time(self):
-		# Temps de jeu cumulé
-		return self.history.cumulate_time()
-
-	def human_cumulate_time(self):
-		# Retourne le temps total joué humainement lisible
-		if self.history.cumulate_time() == timedelta(): # test si le temps cumulate_time() retourne bien un delta et non le caractère "-"
-			return " "
-		delta = humanize.naturaldelta(self.history.cumulate_time())
-		return delta
-
-	def latest_opening_date(self):
-		# Retourne la dernière date où le jeu a ét éouvert
-		return self.history.last_date()
-
-	def latest_opening_duration(self):
-		# Retourne la durée depuis laquelle le jeu a été ouvert
-		if self.latest_opening_date():
-
-			# Réception de la chaine string et transformation en datetime
-			last_date= datetime.strptime(self.history.last_date(), "%Y-%m-%dT%H:%M:%S") 
-			delta=datetime.now() - last_date
-			return delta
-		return None
-
-	def human_latest_opening_duration(self):
-		# Temps depuis la dernière ouverture humainement lisible
-		if self.latest_opening_date():
-			return humanize.naturaldelta(self.latest_opening_duration())
-		return "-"
-
-	def listOfAuthors(self):
-		return formatDataListToLitteralList(self.authors, AUTHOR_VOID_SYMBOL.value)
-
-	def listOfStudios(self):
-		return formatDataListToLitteralList(self.studios, STUDIO_VOID_SYMBOL.value)
-
-	def delete(self):
-		deleteGameFromDatabase(self.code)
-
-	def showPlot(self):
-		pass
-
-
-def create_game_objects():
-	# Création de la liste des jeux
-
-	# Intialisation
-	global listOfGames
-	listOfGames={}
-
-	# Extraction des jeux
-	with open(GAME_FILE.fullPath()) as f:
-		listOfGamesData = json.load(f)["games"]
-
-	## Déploiment des objet de jeux
-	for aGame in listOfGamesData:
-		Game(
-			name=aGame.get("name"),
-			code=aGame.get("code"),
-			licence=get_licence_object_after_code(aGame.get("licence")),
-			url=aGame.get("url"),
-			year=aGame.get("year"),
-			genre=get_genre_object_after_code(aGame.get("genre")),
-			command=aGame.get("command"),
-			authors=aGame.get("authors"),
-			studios=aGame.get("studios"),
-			platform=get_platform_object_after_code(aGame.get("platform")),
-		)
 
 ########################################################################
 # Fonction de l’autocompletion
---
+++ triumphum/games_classes.py
@@ -0,0 +1,174 @@
+########################################################################
+# Classe des jeux
+########################################################################
+import humanize
+from datetime import date, datetime, timedelta
+from tabulate import tabulate
+import json
+
+from triumphum.global_variables import *
+import triumphum.config_file as config_file
+from triumphum.history_classes import retrive_history_of_a_game
+from triumphum.licences_classes import  get_licence_object_after_code
+from triumphum.genres_classes import get_genre_object_after_code
+from triumphum.platforms_classes import get_platform_object_after_code
+import triumphum.symbols as symbols
+
+from triumphum.debug import * # TODO
+
+def formatDataListToLitteralList(list_, voidSymbol):
+	try:
+		n = len(list_)
+	except:
+		n = 0
+	if n == 0:
+		return voidSymbol # TODO
+	elif n == 1:
+		return list_[0]
+	elif n == 2:
+		return f"{list_[0]} et {list_[1]}"
+	else:
+		elements = ", ".join(list_[:-1])
+		return f"{elements}, et {list_[-1]}"
+
+# Défffinition de classe
+class Game:
+	def __init__(self, name=None, code=None, licence=None, url=None, year=None, genre=None, authors=None, studios=[], command=None, comments=None, platform=None):
+		self.name = name
+		self.code = code
+		self.licence = licence
+		self.url = url
+		self.year = year
+		self.genre = genre
+		self.authors = authors
+		self.studios = studios
+		self.command = command
+		self.comments = comments
+		self.platform = platform
+		self.history = self.get_history()
+		self.latest_opening_date_value = self.latest_opening_date()
+		self.playing_duration = self.cumulate_time()
+
+		listOfGames[self.code]=self # Adjonction à la liste des jeux
+
+	def ncurseLine(self):
+		# Préparation de la ligne de tableau
+
+		# Vérifier chaque clé pour une éventuelle valeur vide et remplacer par "-"
+		ncurseLine = [
+			self.platform.abbr or symbols.PLATFORM_VOID_SYMBOL.value,
+			self.name or symbols.NAME_VOID_SYMBOL.value,
+			self.licence.abbr or symbols.LICENCE_VOID_SYMBOL.value,
+			self.genre.abbr or symbols.GENRE_VOID_SYMBOL.value,
+			self.year or symbols.DATE_VOID_SYMBOL.value,
+			self.human_latest_opening_duration() or symbols.LASTOPENING_VOID_SYMBOL.value,
+			self.human_cumulate_time() or symbols.CUMULATEDTIME_VOID_SYMBOL.value,
+			self.listOfAuthors() or symbols.AUTHOR_VOID_SYMBOL.value,
+			self.listOfStudios() or symbols.STUDIO_VOID_SYMBOL.value,
+			self
+		]
+		return ncurseLine
+
+	def asciiRow(self):
+		# Vérifier chaque clé pour une éventuelle valeur vide et remplacer par "-"
+		asciiRow = [
+			self.platform.abbr or config_file.PLATFORM_VOID_SYMBOL.value,
+			self.name or config_file.NAME_VOID_SYMBOL.value,
+			self.licence.abbr or config_file.LICENCE_VOID_SYMBOL.value,
+			self.genre.abbr or config_file.GENRE_VOID_SYMBOL.value,
+			self.year or config_file.DATE_VOID_SYMBOL.value,
+			self.human_latest_opening_duration() or config_file.LASTOPENING_VOID_SYMBOL.value,
+			self.human_cumulate_time() or config_file.CUMULATEDTIME_VOID_SYMBOL.value,
+			self.listOfAuthors() or config_file.AUTHOR_VOID_SYMBOL.value,
+			self.listOfStudios() or config_file.STUDIO_VOID_SYMBOL.value
+		]
+		return asciiRow
+
+	def sheet(self):
+		# Fiche rapide de description de jeu
+		sheet_data=[
+			["Nom", self.name],
+			["code", self.code],
+			["Licence", self.licence.name],
+			["URL", self.url],
+			["Genre", self.genre.name],
+			["Auteur", self.listOfAuthors()],
+			["Commande", self.command],
+			["Dernière ouverture", self.latest_opening_date()],
+		]
+
+		print(tabulate(sheet_data))
+
+	def get_history(self):
+		# Retourne l’historique des dates et heures de parties jouées
+		return retrive_history_of_a_game(self)
+
+	def cumulate_time(self):
+		# Temps de jeu cumulé
+		return self.history.cumulate_time()
+
+	def human_cumulate_time(self):
+		# Retourne le temps total joué humainement lisible
+		if self.history.cumulate_time() == timedelta(): # test si le temps cumulate_time() retourne bien un delta et non le caractère "-"
+			return " "
+		delta = humanize.naturaldelta(self.history.cumulate_time())
+		return delta
+
+	def latest_opening_date(self):
+		# Retourne la dernière date où le jeu a ét éouvert
+		return self.history.last_date()
+
+	def latest_opening_duration(self):
+		# Retourne la durée depuis laquelle le jeu a été ouvert
+		if self.latest_opening_date():
+
+			# Réception de la chaine string et transformation en datetime
+			last_date= datetime.strptime(self.history.last_date(), "%Y-%m-%dT%H:%M:%S") 
+			delta=datetime.now() - last_date
+			return delta
+		return None
+
+	def human_latest_opening_duration(self):
+		# Temps depuis la dernière ouverture humainement lisible
+		if self.latest_opening_date():
+			return humanize.naturaldelta(self.latest_opening_duration())
+		return "-"
+
+	def listOfAuthors(self):
+		return formatDataListToLitteralList(self.authors, symbols.AUTHOR_VOID_SYMBOL.value)
+
+	def listOfStudios(self):
+		return formatDataListToLitteralList(self.studios, symbols.STUDIO_VOID_SYMBOL.value)
+
+	def delete(self):
+		deleteGameFromDatabase(self.code)
+
+	def showPlot(self):
+		pass
+
+
+def create_game_objects():
+	# Création de la liste des jeux
+
+	# Extraction des jeux
+	with open(config_file.GAME_FILE.fullPath()) as f:
+		listOfGamesData = json.load(f)["games"]
+
+	writeInTmp(listOfGamesData)
+
+
+
+	## Déploiment des objet de jeux
+	for aGame in listOfGamesData:
+		Game(
+			name=aGame.get("name"),
+			code=aGame.get("code"),
+			licence=get_licence_object_after_code(aGame.get("licence")),
+			url=aGame.get("url"),
+			year=aGame.get("year"),
+			genre=get_genre_object_after_code(aGame.get("genre")),
+			command=aGame.get("command"),
+			authors=aGame.get("authors"),
+			studios=aGame.get("studios"),
+			platform=get_platform_object_after_code(aGame.get("platform")),
+		)
