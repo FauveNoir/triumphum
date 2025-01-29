31921c838329ad2d256eda85047af533afa98280
FauveNoir <fauve.ordinator@taniere.info>
2025-01-29 02:10:12 +0100

    modularisation des fonctions d’edition de bdd

--- main.py
+++ main.py
@@ -40,7 +40,8 @@ from triumphum.history_classes import  History
 from triumphum.games_classes import  create_game_objects
 from triumphum.autocomplection import listOfAllGamesCodePerLine, listOfAllLicencesCodePerLine, listOfAllGenresCodePerLine, listOfAllPlatformsCodePerLine
 from triumphum.cli_list import printGamesTable, printLicencesTable, printPlatformsTable, printGenresTable
-
+from triumphum.db_edition import addGameToDataBase, addGenreToDataBase, addLicenceToDataBase, addPlatformToDataBase
+from triumphum.db_edition import deleteGameFromDatabase, deleteLicenceFromDatabase, deleteGenreFromDatabase, deletePlatformFromDatabase
 
 verifyConfigFileExistence()
 
@@ -60,102 +61,6 @@ verifyConfigFileExistence()
 
 
 
-########################################################################
-# Éidition des bases de données (nouveau)
-########################################################################
-
-# Fonctions primitives #################################################
-
-def isObjectExistInsideListOfObjects(theObject, listOfObjects):
-	for anObject in listOfObjects:
-		if theObject["code"] == listOfObjects[anObject].code:
-			return True
-	return False
-
-def isNewObjectCodeAllowed(theObject, listOfObjects):
-	if not isObjectExistInsideListOfObjects(theObject, listOfObjects) and theObject["code"] != None:
-		return True
-	return False
-
-def realyAddObjectToDataBase(theObject=None, object_file=None, objectGroupName=None):
-
-	# Charger le contenu JSON depuis le fichier
-	with open(object_file, 'r') as jsonFile:
-		jsonContent = json.load(jsonFile)
-
-	jsonContent[objectGroupName].append(theObject)
-
-	# Réécrire le fichier JSON avec le contenu mis à jour
-	with open(object_file, 'w') as jsonFile:
-		json.dump(jsonContent, jsonFile, indent="\t")
-
-# Fonctions primitive #################################################################
-
-def addObjectToDataBase(theObject=None, listOfObjects=None, object_file=None, objectGroupName=None, object_name=None):
-	if isNewObjectCodeAllowed(theObject, listOfObjects) :
-		realyAddObjectToDataBase(theObject=theObject, object_file=object_file, objectGroupName=objectGroupName)
-	elif isObjectExistInsideListOfObjects(theObject, listOfObjects):
-		print(f"Le code « {theObject['code']} » éxiste déjà.")
-	elif code == None:
-		print(f"Veuillez déffinir un code d’entification pour le {object_name}.")
-
-# Fonctions dérrivées #################################################################
-
-def addGameToDataBase(theObject):
-	addObjectToDataBase(theObject=theObject, listOfObjects=listOfGames, object_file=GAME_FILE.fullPath(), objectGroupName="games", object_name="jeu")
-
-def addGenreToDataBase(theObject):
-	addObjectToDataBase(theObject=theObject, listOfObjects=listOfGenres, object_file=GENRE_FILE.fullPath(), objectGroupName="genres", object_name="genre")
-
-def addLicenceToDataBase(theObject):
-	addObjectToDataBase(theObject=theObject, listOfObjects=listOfLicences, object_file=LICENCE_FILE.fullPath(), objectGroupName="licences", object_name="licence")
-
-def addPlatformToDataBase(theObject):
-	addObjectToDataBase(theObject=theObject, listOfObjects=listOfPlatforms, object_file=PLATFORM_FILE.fullPath(), objectGroupName="platforms", object_name="plateforme")
-
-########################################################################
-# Éidition des bases de données | Délétion (nouveau)
-########################################################################
-
-#
-# Primitive
-#
-
-def deleteObjectFromDatabase(givenObject=None, listOfObjectsFile=None, objectGroupName=None):
-	with open(listOfObjectsFile, 'r') as f:
-		jsonContent = json.load(f)  # Charger le JSON dans une structure de données Python
-
-	founded=False
-	# Vérifier si la clé "games" existe et qu'elle est une liste
-	if objectGroupName in jsonContent and isinstance(jsonContent[objectGroupName], list):
-		list_ = jsonContent[objectGroupName]
-		# Parcourir la liste des jeux
-		for anObject in list_:
-			# Vérifier si l'objet a pour valeur "code": "abc"
-			if isinstance(anObject, dict) and anObject.get('code') == givenObject:
-				founded=True
-				list_.remove(anObject)  # Supprimer l'élément de la liste
-	
-	if founded:
-		# Réécrire le fichier JSON avec les modifications
-		with open(listOfObjectsFile, 'w') as f:
-			json.dump(jsonContent, f, indent='\t')  # Réécrire le JSON avec indentation pour la lisibilité
-
-#
-# Dérrivée
-#
-
-def deleteGameFromDatabase(givenObject):
-	deleteObjectFromDatabase(givenObject=givenObject, listOfObjectsFile=GAME_FILE.fullPath(), objectGroupName="games")
-
-def deleteLicenceFromDatabase(givenObject):
-	deleteObjectFromDatabase(givenObject=givenObject, listOfObjectsFile=LICENCE_FILE.fullPath(), objectGroupName="licences")
-
-def deleteGenreFromDatabase(givenObject):
-	deleteObjectFromDatabase(givenObject=givenObject, listOfObjectsFile=GENRE_FILE.fullPath(), objectGroupName="genres")
-
-def deletePlatformFromDatabase(givenObject):
-	deleteObjectFromDatabase(givenObject=givenObject, listOfObjectsFile=PLATFORM_FILE.fullPath(), objectGroupName="platforms")
 
 
 ########################################################################
