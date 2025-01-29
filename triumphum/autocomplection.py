c1318b0bb0c8ca0288e22ee90718d202733f7f64
FauveNoir <fauve.ordinator@taniere.info>
2025-01-29 01:58:14 +0100

    modularisation de l’autocompletion

---
+++ triumphum/autocomplection.py
@@ -0,0 +1,34 @@
+########################################################################
+# Fonction de l’autocompletion
+########################################################################
+
+def deleteBlancLines(chaine):
+	# Divise la chaîne en lignes individuelles
+	lignes = chaine.splitlines()
+	
+	# Filtre les lignes qui ne sont pas vides
+	lignes_non_vides = [ligne for ligne in lignes if ligne.strip() != '']
+	
+	# Rejoint les lignes non vides en une seule chaîne
+	chaine_sans_lignes_vides = '\n'.join(lignes_non_vides)
+	
+	return chaine_sans_lignes_vides
+
+def listAllObjectsCodeOfClassCodePerLine(listOfObjectsOfClass):
+	listOfCodes=""
+	for anObject in listOfObjectsOfClass:
+#		listOfCodes+="\n" + anObject
+		listOfCodes+="\n" + f"{anObject}[{listOfObjectsOfClass[anObject].name}]"
+	return deleteBlancLines(listOfCodes)
+
+def listOfAllGamesCodePerLine():
+	return listAllObjectsCodeOfClassCodePerLine(listOfGames)
+
+def listOfAllLicencesCodePerLine():
+	return listAllObjectsCodeOfClassCodePerLine(listOfLicences)
+
+def listOfAllGenresCodePerLine():
+	return listAllObjectsCodeOfClassCodePerLine(listOfGenres)
+
+def listOfAllPlatformsCodePerLine():
+	return listAllObjectsCodeOfClassCodePerLine(listOfPlatforms)
