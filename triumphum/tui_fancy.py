862a0da5e35391208f3d1b9f3ac4a1ec93868ab2
FauveNoir <fauve.ordinator@taniere.info>
2025-01-30 02:23:26 +0100

    supression de tui_fancy.py

--- triumphum/tui_fancy.py
+++
@@ -1,6 +0,0 @@
-########################################################################
-# Fonctions ésthétiques de l’interface interactive
-########################################################################
-from triumphum.global_variables import *
-import triumphum.global_variables as global_variables
-
