d4350c83e0ca3887b695ae896ff1652fe49b3459
FauveNoir <fauve.ordinator@taniere.info>
2025-01-30 20:08:36 +0100

    Bump version: 1.0.0 → 1.0.1

--- triumphum/__init__.py
+++ triumphum/__init__.py
@@ -8,7 +8,7 @@ import humanize
 APP_CODE_NAME="triumphum"
 APP_FANCY_NAME="Triumphum"
 APP_DESCRIPTION="Gestionnaire de ludothèque en Python et NCurses pour GNU/Linux"
-APP_VERSION="1.0.0"
+APP_VERSION="1.0.1"
 APP_AUTHOR="Fauve"
 APP_LICENCE=""
 APP_MOTO="LACRIMOSA·GAVDIVM·EST"
