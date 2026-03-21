aeeb425b1c8bd073ede2ded2c2162096880da83e
FauveNoir <fauve.ordinator@taniere.info>
2026-03-21 19:37:12 +0000

    Commentaire de triumphum/global_variables.py

--- triumphum/global_variables.py
+++ triumphum/global_variables.py
@@ -6,8 +6,6 @@ listOfPlatforms={} # Dictionnaire des plateformes disponibles
 listOfGenres={} # Dicitonnaire des genres disponibles
 listOfLicences={} # Dicitonnaire des licences disponibles
 listOfGames={} # Dicitonnaire des jeux disponibles
-THE_VISUAL_LIST_OF_GAMES=None
-titles = [" ", "Titre", "Licence", "Genre", "Date", "Dernière ouverture", "Temps cumulé", "Auteur", "Studio"]
-#items=[] # Liste des items dans la liste
+THE_VISUAL_LIST_OF_GAMES=None # L’objet liste visuelle
+titles=[" ", "Titre", "Licence", "Genre", "Date", "Dernière ouverture", "Temps cumulé", "Auteur", "Studio"]
 STDSCR=None
-
