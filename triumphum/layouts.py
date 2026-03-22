1140849aab41124b53eeaeebc6f3932b9d84e258
FauveNoir <fauve.ordinator@taniere.info>
2026-03-22 01:28:46 +0000

    Commentaire de triumphum/layouts.py

--- triumphum/layouts.py
+++ triumphum/layouts.py
@@ -7,114 +7,119 @@ from triumphum.misc import getElementHavingParameterWithValue
 from triumphum.debug import * # TODO
 
 def getListOfAParametterFromAListOfObjects(givenList=None, parameter=None):
-	if parameter is None or parameter is None:
-		return None
+    # Cette fonction fait en sorte de retourner tous les paramettres ayant pour nom `parameter`, des items contenus dans la liste `givenList`.
+    # Exemple : si `givenList` contient [foo, bar, baz], et que `parameter` donné en entrée est `xyz`, alors la fonction retourne une liste qui serait équivalente à [foo.xyz, bar.xyz, baz.xyz].
 
-	outputList=[]
-	for anElement in givenList:
-		outputList.append(getattr(anElement, parameter))
-	return outputList
+    # Cette fonction est particulièrement utile pour foreacher au sein de `listOfBindings` tous les bindings qui y éxistent et en récupérer l’attribut `code`.
+    if parameter is None or parameter is None:
+        return None
+
+    outputList=[]
+    for anElement in givenList:
+        outputList.append(getattr(anElement, parameter))
+    return outputList
 
 
 class Layout:
-	# Classe des disposition de clavier ayant chacune son propre jeu de binding
-	# fancyName : Nom littéral tel qu’il apparaitra à l’utilisateur lorsqu’une représentation linguistique est permise
-	# code : code de la disposition qui sert 1. dans la variable locale de l’objet 2. Que l’utilisateur utilesra lors de ses configurations et appels à la ligne de commande
-	# **attributs : Autres paramettres sensés être générés d’après la liste des racourcis actyliques disponibles
-	def __init__(self, fancyName=None, code=None, **attributs):
-		self.fancyName=fancyName
-		self.code=code
+    # Classe des disposition de clavier ayant chacune son propre jeu de binding
+    # fancyName : Nom littéral tel qu’il apparaitra à l’utilisateur lorsqu’une représentation linguistique est permise
+    # code : code de la disposition qui sert 1. dans la variable locale de l’objet 2. Que l’utilisateur utilesra lors de ses configurations et appels à la ligne de commande
+    # **attributs : Autres paramettres sensés être générés d’après la liste des racourcis actyliques disponibles
+    def __init__(self, fancyName=None, code=None, **attributs):
+        self.fancyName=fancyName
+        self.code=code
 
-		# Enregistrer tous les attributs supplémentaires de **attributs comme autant de paramettres
-		# TODO limiter les attributs à la liste des racourcis dactyliques disponibles
-		for attributName, value in attributs.items():
-			setattr(self, attributName, value)
+        # Enregistrer tous les attributs supplémentaires de **attributs comme autant de paramettres
+        # TODO limiter les attributs à la liste des racourcis dactyliques disponibles
+        for attributName, value in attributs.items():
+            setattr(self, attributName, value)
 
-		global_variables.listOfLayouts[self.code]=self
+        global_variables.listOfLayouts[self.code]=self
 
-	def apply(self):
-	# Appliquer les associations de la disposition et utiliser ses racourcis dactyliques
-		for aKey in getListOfAParametterFromAListOfObjects(givenList=global_variables.listOfBindings, parameter="code"):
-		# Parcour `listOfBindings` pour ittérer sur chacun des paramettres `.code` des éléments qu’elle contient.
-			if hasattr(self, aKey):
-				value = getattr(self, aKey)
-				# ↓ Récupére le Binding ayant pour `.code` la valeur de `aKey`
-				theBinding=getElementHavingParameterWithValue(givenList=global_variables.listOfBindings, parameter="code", value=aKey)
-				theBinding.setKey(value)
+    def apply(self):
+    # Appliquer les associations de la disposition et utiliser ses racourcis dactyliques
+        for aKey in getListOfAParametterFromAListOfObjects(givenList=global_variables.listOfBindings, parameter="code"):
+        # Parcour `listOfBindings` pour ittérer sur chacun des paramettres `.code` des éléments qu’elle contient.
+            if hasattr(self, aKey):
+                value = getattr(self, aKey)
+                # ↓ Récupére le Binding ayant pour `.code` la valeur de `aKey`
+                theBinding=getElementHavingParameterWithValue(givenList=global_variables.listOfBindings, parameter="code", value=aKey)
+                theBinding.setKey(value)
 
 #
 ## Déffinition des dispositions disponibles
 #
 
 def makeLayoutsList():
-	Layout(fancyName="BÉPO", code="bepo",
-		bindGoDown="t",
-		bindGoUp="s",
-		bindRunGame="\n",
-		bindSortByName="b",
-		bindSortByLicence="é",
-		bindSortByGenre="p",
-		bindSortByDate="o",
-		bindSortByLastOpening="è",
-		bindSortByPlayingDuration="v",
-		bindSortByPlatform="!",
-		bindOpenLink="A",
-		bindEditData="e",
-	#	bindDelete="d",
-		bindComment="i",
-		bindMakeDonation="x",
-		bindShowFullLicence="w",
-		bindFilter="/",
-		bindSeeBindingHelp="h",
-		bindCopyLink="y",
-		bindRefreshScreen="l",
-		bindQuit="q"
-		)
+    # Déploiement des layouts éxistants
+    Layout(fancyName="BÉPO", code="bepo",
+        bindGoDown="t",
+        bindGoUp="s",
+        bindRunGame="\n",
+        bindSortByName="b",
+        bindSortByLicence="é",
+        bindSortByGenre="p",
+        bindSortByDate="o",
+        bindSortByLastOpening="è",
+        bindSortByPlayingDuration="v",
+        bindSortByPlatform="!",
+        bindOpenLink="A",
+        bindEditData="e",
+    #    bindDelete="d",
+        bindComment="i",
+        bindMakeDonation="x",
+        bindShowFullLicence="w",
+        bindFilter="/",
+        bindSeeBindingHelp="h",
+        bindCopyLink="y",
+        bindRefreshScreen="l",
+        bindQuit="q"
+        )
 
-	Layout(fancyName="AZERTY", code="azerty",
-		bindGoDown="j",
-		bindGoUp="k",
-		bindRunGame="\n",
-		bindSortByName="a",
-		bindSortByLicence="z",
-		bindSortByGenre="e",
-		bindSortByDate="r",
-		bindSortByLastOpening="t",
-		bindSortByPlayingDuration="y",
-		bindSortByPlatform="o",
-		bindOpenLink="A",
-		bindEditData="f",
-	#	bindDelete="d",
-		bindComment="s",
-		bindMakeDonation="c",
-		bindShowFullLicence="p",
-		bindFilter="/",
-		bindSeeBindingHelp="h",
-		bindCopyLink="y",
-		bindRefreshScreen="l",
-		bindQuit="q"
-		)
+    Layout(fancyName="AZERTY", code="azerty",
+        bindGoDown="j",
+        bindGoUp="k",
+        bindRunGame="\n",
+        bindSortByName="a",
+        bindSortByLicence="z",
+        bindSortByGenre="e",
+        bindSortByDate="r",
+        bindSortByLastOpening="t",
+        bindSortByPlayingDuration="y",
+        bindSortByPlatform="o",
+        bindOpenLink="A",
+        bindEditData="f",
+    #    bindDelete="d",
+        bindComment="s",
+        bindMakeDonation="c",
+        bindShowFullLicence="p",
+        bindFilter="/",
+        bindSeeBindingHelp="h",
+        bindCopyLink="y",
+        bindRefreshScreen="l",
+        bindQuit="q"
+        )
 
-	Layout(fancyName="QWERTY", code="qwerty",
-		bindGoDown="j",
-		bindGoUp="k",
-		bindRunGame="\n",
-		bindSortByName="q",
-		bindSortByLicence="w",
-		bindSortByGenre="e",
-		bindSortByDate="r",
-		bindSortByLastOpening="t",
-		bindSortByPlayingDuration="y",
-		bindSortByPlatform="u",
-		bindOpenLink="A",
-		bindEditData="f",
-	#	bindDelete="d",
-		bindComment="s",
-		bindMakeDonation="c",
-		bindShowFullLicence="p",
-		bindFilter="/",
-		bindSeeBindingHelp="h",
-		bindCopyLink="y",
-		bindRefreshScreen="l",
-		bindQuit="x"
-		)
+    Layout(fancyName="QWERTY", code="qwerty",
+        bindGoDown="j",
+        bindGoUp="k",
+        bindRunGame="\n",
+        bindSortByName="q",
+        bindSortByLicence="w",
+        bindSortByGenre="e",
+        bindSortByDate="r",
+        bindSortByLastOpening="t",
+        bindSortByPlayingDuration="y",
+        bindSortByPlatform="u",
+        bindOpenLink="A",
+        bindEditData="f",
+    #    bindDelete="d",
+        bindComment="s",
+        bindMakeDonation="c",
+        bindShowFullLicence="p",
+        bindFilter="/",
+        bindSeeBindingHelp="h",
+        bindCopyLink="y",
+        bindRefreshScreen="l",
+        bindQuit="x"
+        )
