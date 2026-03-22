c906805fa594ed85123dd4848dfb069be4547e92
FauveNoir <fauve.ordinator@taniere.info>
2026-03-22 03:15:56 +0100

    Commentaire de triumphum/platforms_classes.py

--- triumphum/platforms_classes.py
+++ triumphum/platforms_classes.py
@@ -8,55 +8,61 @@ import triumphum.symbols as symbols
 
 # défffinition de classe
 class Platform:
-	def __init__(self, name=None, code=None, abbr=None, includeInSorting=True):
-		self.name = name
-		self.code = code
-		self.abbr = abbr
-		self.includeInSorting = includeInSorting
+    def __init__(self, name=None, code=None, abbr=None, includeInSorting=True):
+        self.name = name
+        self.code = code
+        self.abbr = abbr
+        self.includeInSorting = includeInSorting
 
-		listOfPlatforms[self.code]=self
-	def __eq__(self, other):
-		if isinstance(other, Platform):
-			return self.abbr == other.abbr
-		return NotImplemented
+        listOfPlatforms[self.code]=self # Adjonction à la liste des plateformes
 
-	def __lt__(self, other):
-		if isinstance(other, Platform):
-			return self.abbr <  other.abbr
-		return NotImplemented
+    def asciiRow(self):
+        # Retourne la cellule particulière pour la construction de la table des jeux
+        # Vérifier chaque clé pour une éventuelle valeur vide et remplacer par "-"
+        asciiRow = [
+            self.name or symbols.GENERAL_VOID_SYMBOL,
+            self.abbr or symbols.GENERAL_VOID_SYMBOL,
+        ]
+        return asciiRow
 
-	def __gt__(self, other):
-		if isinstance(other, Platform):
-			return self.abbr > other.abbr
-		return NotImplemented
+    # Blocs de comparaisons permétant de trier les plateformes entre elles d’après le paramettre .abbr
+    def __eq__(self, other):
+        if isinstance(other, Platform):
+            return self.abbr == other.abbr
+        return NotImplemented
 
-	def asciiRow(self):
-		# Vérifier chaque clé pour une éventuelle valeur vide et remplacer par "-"
-		asciiRow = [
-			self.name or symbols.GENERAL_VOID_SYMBOL,
-			self.abbr or symbols.GENERAL_VOID_SYMBOL,
-		]
-		return asciiRow
+    def __lt__(self, other):
+        if isinstance(other, Platform):
+            return self.abbr <  other.abbr
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Platform):
+            return self.abbr > other.abbr
+        return NotImplemented
 
 def create_platform_objects():
-	# Création de la liste des plateformes disponibles
+    # Création de la liste des plateformes disponibles
 
-	# Extraction des plateformes
-	with open(config_file.PLATFORM_FILE.fullPath()) as f:
-		listOfPlatformsData = json.load(f)["platforms"]
+    # Extraction des plateformes
+    with open(config_file.PLATFORM_FILE.fullPath()) as f:
+        listOfPlatformsData = json.load(f)["platforms"]
 
-	# Déploiment des objet de licence
-	for aPlatform in listOfPlatformsData:
-		Platform(
-			name=aPlatform.get("name"),
-			code=aPlatform.get("code"),
-			abbr=aPlatform.get("abbr")
-		)
+    # Déploiment des objet de licence
+    for aPlatform in listOfPlatformsData:
+        Platform(
+            name=aPlatform.get("name"),
+            code=aPlatform.get("code"),
+            abbr=aPlatform.get("abbr")
+        )
 
+# Objet spécifique de plateforme inconue
 unknownplatform=Platform(name="Plateforme inconue", code="unknownplatform", abbr="", includeInSorting=False)
 
-def get_platform_object_after_code(code):
-	if code in listOfPlatforms:
-		return listOfPlatforms[code]
-	return unknownplatform
+def get_platform_object_after_code(code=None):
+    # fonction retournant l’objet de plateforme d’après le code fourni en entrée
+    # Si rien n’y correspond, retourne l’objet spécial `unknownplatform`
+    if code in listOfPlatforms:
+        return listOfPlatforms[code]
+    return unknownplatform
 
